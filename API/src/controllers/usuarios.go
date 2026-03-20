9877da3fa9e7228abd5bd7e2bf810fc3cd6e9a7d
Douglas Lima <95377746+lima-doug@users.noreply.github.com>
2026-03-19 22:57:29 -0300

    WIP

--- API/src/controllers/usuarios.go
+++ API/src/controllers/usuarios.go
@@ -5,6 +5,7 @@ import (
 	"api/src/modelos"
 	"api/src/repositorios"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -27,8 +28,12 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-	repositorio.Criar(usuario)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
 
+	w.Write([]byte(fmt.Sprintf("Id inserio: %d", usuarioID)))
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
--- API/src/repositorios/usuarios.go
+++ API/src/repositorios/usuarios.go
@@ -3,6 +3,7 @@ package repositorios
 import (
 	"api/src/modelos"
 	"database/sql"
+	"log"
 )
 
 type usuarios struct {
@@ -14,5 +15,26 @@ func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
 }
 
 func (u usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
-	return 0, nil
+	
+	statement, erro := u.db.Prepare(
+		"insert into usuarios (nome, nick, email, senha) values(?,?,?,?)"
+	)
+	if erro != nil {
+		return 0, erro
+	}
+
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil{
+		return 0, erro
+	}
+	
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro := nil{
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido)
+	
 }
